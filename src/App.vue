51a7751e510ec6befbef51c1aba571a26accde76
Gustavo A Ronconi <gustavoronconi95@gmail.com>
2021-07-06 11:04:09 -0300

    frontend simplecapp

--- src/App.vue
+++ src/App.vue
@@ -19,10 +19,14 @@ export default defineComponent({
 @import url("https://fonts.googleapis.com/css2?family=Roboto&display=swap");
 body {
   font-family: "Roboto";
+  padding-top: 105px;
 }
 .btn-simplecapp {
   color: #3f3d3d !important;
   background-color: #f7de50 !important;
   border-color: #f7de50 !important;
+}
+.btn-simplecapp:active {
+  color: #b19600 !important;
 }
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg fixed-top">
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container-fluid ms-7">
       <img src="/img/logo.png" alt="" class="brand" />
       <button
@@ -14,9 +14,11 @@
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ms-auto">
+        <ul class="navbar-nav ms-auto me-5 list-group">
           <li class="nav-item">
-            <a class="nav-link" href="#">Contato</a>
+            <a class="nav-link nav-bar-links" href="#"
+              ><i class="fas fa-phone-square-alt space-ico"></i>Contato
+            </a>
           </li>
           <li class="nav-item dropdown">
             <a
@@ -27,13 +29,12 @@
               data-bs-toggle="dropdown"
               aria-expanded="false"
             >
-              Sobre
+              <i class="fas fa-users space-ico"></i>Redes Socials
             </a>
             <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider" /></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
+              <li><a class="dropdown-item" href="#"><i class="fab fa-linkedin space-ico"></i>Linkedin</a></li>
+              <li><a class="dropdown-item" href="#"><i class="fab fa-facebook-square space-ico"></i>Facebook</a></li>
+              <li><a class="dropdown-item" href="#"><i class="fab fa-instagram space-ico"></i>Instagram</a></li>
             </ul>
           </li>
         </ul>
@@ -54,15 +55,26 @@ export default defineComponent({
 
 <style scoped lang="scss">
 .brand {
-  max-height: 100px;
-}
-#navbarSupportedContent {
-  margin-left: 30px;
+  max-height: 75px;
 }
 .navbar {
   background-color: #f7de50;
+  border-bottom: 1px solid rgb(202, 200, 200);
 }
 .ms-7 {
-  margin-left: 75px !important;
+  margin-left: 3% !important;
+}
+.nav-link {
+  color: #4b4949 !important;
+  font-size: 20px;
+}
+.space-ico {
+  margin-right: 5px;
+}
+.nav-item {
+  margin-right: 15px;
+}
+.dropdown-menu {
+  background-color: #f7de50;
 }
 </style>
